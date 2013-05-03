4f03ca5baf9cea5496dd5f6e8a803835b79eeeda
a.m. hasler <amhasler@gmail.com>
2013-05-03 10:13:41 -0400

    add media queries and made contact record email

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,15 +2,18 @@ class ContactController < ApplicationController
 
   def new
     @message = Message.new
+    @email = Email.new
   end
 
   def create
 
     @message = Message.new(params[:message])
+    @email = Email.new(params[:email])
     
 
     if @message.valid?
       NotificationsMailer.new_message(@message).deliver
+      @email.save
       redirect_to(root_path, :notice => "Message was successfully sent.")
     else
       flash.now.alert = "Please fill all fields."
