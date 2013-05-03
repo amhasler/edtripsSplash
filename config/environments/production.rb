a7842c7e72fe13ad3fadf6f3a6b592ad484937dd
a.m. hasler <amhasler@gmail.com>
2013-05-03 00:25:53 -0400

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ Splash::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  config.assets.initialize_on_precompile = false
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
@@ -64,5 +66,4 @@ Splash::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.assets.precompile += ['static_pages.css']
 end
